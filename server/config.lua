48329bf396de16f11bf58e88cfe41ed6dbcf4f96
yungege <yungege@qq.com>
2018-10-24 01:36:03 +0800

    增加服务器启动脚本，架个空服务器

---
+++ server/config.lua
@@ -0,0 +1,20 @@
+-- preload = "./examples/preload.lua"	-- run preload.lua before every lua service run
+thread = 8
+logger = "skynet.log"
+logpath = "."
+harbor = 0
+start = "main"	-- main script
+bootstrap = "snlua bootstrap"	-- The service for bootstrap
+
+root = "./skynet/"
+luaservice = root.."service/?.lua;"..root.."test/?.lua;"..root.."examples/?.lua;"..root.."test/?/init.lua"
+lualoader = root .. "lualib/loader.lua"
+lua_path = root.."lualib/?.lua;"..root.."lualib/?/init.lua"
+lua_cpath = root .. "luaclib/?.so"
+snax = root.."examples/?.lua;"..root.."test/?.lua"
+
+
+-- snax_interface_g = "snax_g"
+luaservice = "./service/?.lua;./service/?/main.lua;"..luaservice
+cpath = root.."cservice/?.so"
+daemon = "./skynet.pid"
---
+++ server/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./skynet/skynet config.lua
